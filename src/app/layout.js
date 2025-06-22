ef8b135c12904871379b95650b06dfd251a4a571
Alim Suma <sumaalim@gmail.com>
2025-06-22 23:41:09 +0800

    update favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,29 +2,37 @@ import { Geist, Geist_Mono, Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter', // custom variable
-  display: 'swap',
-})
+  subsets: ["latin"],
+  variable: "--font-inter", // custom variable
+  display: "swap",
+});
 
 const space = Space_Grotesk({
-  subsets: ['latin'],
-  variable: '--font-space',
-  display: 'swap',
-})
-
+  subsets: ["latin"],
+  variable: "--font-space",
+  display: "swap",
+});
 
 export const metadata = {
   title: "Secrely",
-  description: "Something Apps",
+  description: "Get Your Secrets Out of Your Head and Into a Secure Place",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable} ${space.variable} antialiased`}
-      >
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="description" content={metadata.description} />
+        <meta name="author" content="Whyriez" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://secrely-app.vercel.app" />
+      </head>
+      <body className={`${inter.variable} ${space.variable} antialiased`}>
         {children}
       </body>
     </html>
